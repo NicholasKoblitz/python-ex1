4a4a08202d23983f7c2a01c8fe386acbffd7c504
NicholasK <nicholask1@outlook.com>
2021-12-05 13:58:48 -0500

    Created docstring for words.py

--- words.py
+++ words.py
@@ -1,8 +1,10 @@
 def print_upper_words(words, starts_with):
+    """Prints out all words in a list that starts with the given set."""
+
     for elemnet in starts_with:
         for word in words:
             if word[0].lower() == elemnet.lower():
-                print(word)
+                print(word.upper())
 
 
 print_upper_words(["hello", "hey", "goodbye", "yo", "yes"],
